24a59f15ab859b00c11e16e5c83a000f954ecc03
Derick Young <young.derickj@gmail.com>
2022-03-24 15:03:13 -0600

    Update curlrequest.sh

--- curlrequest.sh
+++ curlrequest.sh
@@ -1,3 +1,4 @@
 curl http://localhost:5000/startGame?name=Derick
 curl http://localhost:5000/chooseClass?role=Warrior
 curl http://localhost:5000/Golem/reward
+curl http://localhost:5000/TreasureChest
